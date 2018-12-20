541045b0b18ee8313c4e2f40ae302a1a136b4ff6
Your Name <you@example.com>
2018-12-20 18:59:09 +0200

    Main to start all tasks

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+//		task2 n=new task2(315);		
+//	    n.primeFactors();
+//	    System.out.println(n);
+		
+		
+//		
+//		
+//String str = "journaldev";
+//		//Copy string characters to char array
+//		char[] chars1 = new char[100];
+//		str.getChars(0, 7, chars1, 0);
+//		System.out.println(chars1);
+//		System.out.print( "Check for Polindrom : ");
+//		task5 p =  new task5();
+//		System.out.print(p.istPalindrom(chars1));
+		
+//		
+//	task7 q=new task7();
+	//q.factorial();
+		
+	}
+
+}
